588ba93c9f76292cb26411fd016a65668db1fec4
prabhasha Rodrigo <66826356+prabhasha1115@users.noreply.github.com>
2025-11-23 09:02:29 +0530

    customer-user-controller-update

---
+++ pos-system-devops/Controller/CustomerController.js
@@ -0,0 +1,90 @@
+const CustomerSchema = require('../model/CustomerSchema');
+
+const createCustomer = async (req,resp) => {
+    
+    try{
+
+    const {name,address,salary,contact} = req.body;
+    const createdCustomer = await CustomerSchema.create({
+        name, address, salary, contact
+    });
+    await createdCustomer.save();
+    resp.state(201).json({message:'Customer saved...'});
+
+    }catch (e) {
+
+        resp.status(500).json({'message':'signup error',error:e})
+
+    }
+    
+};
+const updateCustomer = async (req,resp) => {
+    try{
+
+    const {name,address,salary,contact} = req.body;
+    
+    const updatedData = CustomerSchema.findByIdAndUpdate({_id: req.params.id}, 
+        {name:name,address:address,salary:salary,contact:contact}, {new:true});
+
+        if(!updatedData) return resp.status(500).json({'message':'Try again'});
+
+
+    resp.state(201).json({message:'Customer updated...'});
+
+    }catch (e) {
+
+        resp.status(500).json({'message':'signup error',error:e})
+
+    }
+};
+const deleteCustomer = async (req,resp) => {
+    try{
+
+    
+    const updatedData = CustomerSchema.findByIdAndDelete({_id: req.params.id}); 
+
+        if(!updatedData) return resp.status(500).json({'message':'Try again'});
+
+
+    resp.state(204).json({message:'Customer deleted...'});
+
+    }catch (e) {
+
+        resp.status(500).json({'message':'signup error',error:e})
+
+    }
+};
+const findCustomerById = async (req,resp) => {
+    try{
+
+    
+    const selectedCustomer = CustomerSchema.findOne({_id: req.params.id});
+
+        if(!selectedCustomer) return resp.status(404).json({'message':'Not found'});
+
+
+    resp.state(200).json({message:'Customer data', data:selectedCustomer});
+
+    }catch (e) {
+
+        resp.status(500).json({'message':'signup error',error:e})
+
+    }
+};
+const loadAllCustomer = async (req,resp) => {
+    try{
+
+    
+    const customers = CustomerSchema.find();
+
+
+    resp.state(201).json({message:'Customer data', dataList:customers});
+
+    }catch (e) {
+
+        resp.status(500).json({'message':'signup error',error:e})
+
+    }
+};
+
+module.exports = {createCustomer, updateCustomer, deleteCustomer, findCustomerById, loadAllCustomer};
